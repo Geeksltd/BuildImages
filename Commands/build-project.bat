080a09fd714ebc521db8a86734929d28d9bf7f38
G\Matt <matt@geeks.ltd.uk>
2018-12-07 16:22:44 +0000

    Building only in debug mode.

--- Commands/build-project.bat
+++ Commands/build-project.bat
@@ -1,8 +1,3 @@
-echo build mode : %1
-if %1 ==publish ( 
-	.\Build.bat -publish
-	mklink /J output publish
-	 ) ELSE ( 
-	 .\Build.bat
-	 mklink /J output website
-	 )
+remove-gcop-references 
+
+call .\Build.bat
